46d14647781c3af401894e8a0669564059652ca2
draganmel <draganmel@gmail.com>
2023-04-20 08:23:04 -0700

    initial header componet using rect-bostrep

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Navbar} from 'react-bootstrap';
+
+const Header = (/*props*/ {title}) => {
+    //const { title} = props;
+    return(
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Navbar>
+    )
+};
+export default Header;
