d5e114414edbcf5a7b786dbcc6c68d7a7c8adfb7
draganmel <draganmel@gmail.com>
2023-04-20 17:30:14 -0700

    controlled search component added useState

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Container, Navbar} from 'react-bootstrap';
+
+const navbarStyle = {
+  backgroundColor: 'lightblue'
+};
 
 const Header = (/*props*/ {title}) => {
-    //const { title} = props;
+    //const { title} = props; 
     return(
-        <Navbar bg="light" variant="light">
-          <Navbar.Brand href="/">{title}</Navbar.Brand>
-      </Navbar>
+        <Navbar style={navbarStyle}  variant="light">
+          <Container>
+            <Navbar.Brand href="/">{title}</Navbar.Brand>
+          </Container>
+        </Navbar>
     )
 };
 export default Header;
