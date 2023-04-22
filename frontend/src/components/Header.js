7cfd07914caf734c681626ff90b8edc95aed59c7
draganmel <draganmel@gmail.com>
2023-04-22 10:24:34 -0700

    affeter adding "endOfLine": "lf" cd error disaperad

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {Container, Navbar} from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 
 const navbarStyle = {
-  backgroundColor: 'lightblue'
+  backgroundColor: 'lightblue',
 };
 
-const Header = (/*props*/ {title}) => {
-    //const { title} = props; 
-    return(
-        <Navbar style={navbarStyle}  variant="light">
-          <Container>
-            <Navbar.Brand href="/">{title}</Navbar.Brand>
-          </Container>
-        </Navbar>
-    )
+const Header = (/*props*/ { title }) => {
+  //const { title} = props;
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 export default Header;
