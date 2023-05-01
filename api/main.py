3b98d2866606ecc41996aded33f4f41e2faa3c79
draganmel <draganmel@gmail.com>
2023-05-01 13:36:22 -0700

    start Python API

--- api/main.py
+++ api/main.py
@@ -1,1 +1,13 @@
- 
+# save this as app.py
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/new-image")
+def new_image():
+word=request.args.get("query")
+return{"word" : word}
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0", port=5050)
+    
